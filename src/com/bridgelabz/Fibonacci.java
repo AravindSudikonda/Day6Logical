fc6bbb0cfa7d46c8fefc67a707fb38ed2d6a002e
AravindSudikonda <aravindsudikonda10@example.com>
2022-05-26 15:18:59 +0530

    added Fibonacci pgm

--- src/com/bridgelabz/Fibonacci.java
+++ src/com/bridgelabz/Fibonacci.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.print("Hello Fibonacci");
+        {
+            int n1=0, n2=1, n3;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter a number");
+            int num = scanner.nextInt();
+            System.out.print(n1 + " " + n2);
+            for (int i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                System.out.print(" " + n3);
+                n1 = n2;
+                n2 = n3;
+            }
+        }
     }
 }
-
